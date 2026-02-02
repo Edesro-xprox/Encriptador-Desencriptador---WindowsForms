755187eb883108a70651302566b66f56163a5e63
Edson Espinoza <edesro1996@gmail.com>
2026-02-02 14:14:02 -0500

    Subir archivo gitignore

--- obj/Debug/net6.0-windows/xprox_encryptor_decryptor.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/xprox_encryptor_decryptor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xprox_encryptor_decryptor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d995a669951e2a9f45f787362e9bdd579b5594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5268179e30dc4502a1681cd8048b38cdee7c6287")]
 [assembly: System.Reflection.AssemblyProductAttribute("xprox_encryptor_decryptor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xprox_encryptor_decryptor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
